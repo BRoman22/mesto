b4ed0256d84e1667db9a66fa8bab25b92b4b357a
Roman Bondarev <Rbondarev22@gmail.com>
2023-04-28 17:44:46 +0300

    fix bugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,40 @@
 const openPopup = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
 const closePopup = popup.querySelector('.popup__close');
-const inputName = popup.querySelector('.popup__input');
-const inputBio = popup.querySelectorAll('.popup__input')[1];
+const inputName = popup.querySelector('input[name="user-name"]');
+const inputBio = popup.querySelector('input[name="user-bio"]');
 const profileName = document.querySelector('.profile__title');
 const profileBio = document.querySelector('.profile__subtitle');
 const profileAvatar = document.querySelector('.profile__avatar');
 
-// Функция открывает/закрывает попап
-const openClosePopup = () => {
-  popup.classList.toggle('popup_opened');
-};
+// Функция присваивает/удаляет класс елементу
+const togglePopup = (e) => e.classList.toggle('popup_opened');
 
 // Функция присваивает текущее имя и био пользователя в инпуты попапа
 const addInputsValue = () => {
   inputName.value = profileName.textContent;
   inputBio.value = profileBio.textContent;
-  profileAvatar.alt = profileName.textContent;
 };
 
 // Функция открывает попап и присваивает текущее имя и био пользователя в инпуты попапа
 openPopup.addEventListener('click', () => {
-  openClosePopup();
+  togglePopup(popup);
   addInputsValue();
 });
 
 // Функция закрывает попап
-closePopup.addEventListener('click', openClosePopup);
+closePopup.addEventListener('click', () => togglePopup(popup));
 
-// Фунцкия присваивает новое имя и био пользователя, меняет альт для аватара
-popup.addEventListener('submit', (e) => {
+popup.addEventListener('click', (e) => e.target === e.currentTarget ? togglePopup(popup) : '');
+
+// Фунцкия присваивает новое имя и био пользователя, присваивает альт для аватара на введеное имя
+const changeProfile = (e) => {
   e.preventDefault();
   profileName.textContent = inputName.value;
-  profileAvatar.alt = inputBio.value;
   profileBio.textContent = inputBio.value;
-  openClosePopup();
-});
+  profileAvatar.alt = inputName.value;
+  togglePopup(popup);
+};
+
+// Фунцкия присваивает новое имя и био пользователя, присваивает альт для аватара на введеное имя
+popup.addEventListener('submit', changeProfile);

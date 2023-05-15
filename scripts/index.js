997f52f9681d6b854e8d0fa767e29e968ed6dd6f
Roman Bondarev <Rbondarev22@gmail.com>
2023-05-15 20:44:29 +0300

    refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,20 @@
 const profilePopup = document.querySelector('.popup_profile');
 //попап добавления карточки
 const cardPopup = document.querySelector('.popup_card');
-//попап карточки
-const imagePopup = document.querySelector('.popup_image');
+//попап картинки
+const picturePopup = document.querySelector('.popup_picture');
 //кнопка открытия попапа профиля
-const openProfile = document.querySelector('.profile__button-edit');
+const buttonOpenProfilePopup = document.querySelector('.profile__button-edit');
 //кнопка открытия попапа добавления карточки
-const openCard = document.querySelector('.profile__button-add');
+const buttonOpenCardPopup = document.querySelector('.profile__button-add');
 //все кнопки закрытия попапов
-const closePopups = document.querySelectorAll('.popup__close');
+const buttonClosePopups = document.querySelectorAll('.popup__close');
 //форма профиля
 const formProfile = document.querySelector('.popup__form_profile');
 //форма добавления карточки
 const formCard = document.querySelector('.popup__form_card');
+//шаблон карточки
+const cardTemplate = document.querySelector('#card').content;
 //строка ввода имени попапа профиля
 const inputProfileName = formProfile.querySelector('.popup__input_name');
 //строка ввода о себе попапа профиля
@@ -29,106 +31,76 @@ const profileBio = document.querySelector('.profile__subtitle');
 //елемент куда добавляются карточки
 const cardsList = document.querySelector('.cards__list');
 
-//функция переключения класса, принимает елемент и класс
-const toggleClass = (element, classElement) =>
-  element.classList.toggle(classElement);
+//функция открытия попапа
+const handleOpenPopup = (e) => e.classList.add('popup_opened');
+//функция закрытия попапа
+const handleClosePopup = (e) => e.classList.remove('popup_opened');
 //функция заполения инпутов попапа профиля со страницы
-const addInputsValue = () => {
+const addInputsData = () => {
   inputProfileName.value = profileName.textContent;
   inputProfileBio.value = profileBio.textContent;
-};
-//функция обнуления инпутов попапа карточки
-const clearInputs = () => {
-  inputCardName.value = '';
-  inputCardLink.value = '';
 };
 //функция изменения имени и о себе на странице
 const changeProfile = (e) => {
   e.preventDefault();
   profileName.textContent = inputProfileName.value;
   profileBio.textContent = inputProfileBio.value;
-  toggleClass(profilePopup, 'popup_opened');
+  handleClosePopup(profilePopup);
 };
 //функция открытия попапа картинки
-const openImagePopup = (title, image) => {
-  imagePopup.querySelector('.popup__title_image').textContent =
-    title.textContent;
-  imagePopup.querySelector('.popup__image').src = image.src;
-  imagePopup.querySelector('.popup__image').alt = image.alt;
-  toggleClass(imagePopup, 'popup_opened');
+const openPicturePopup = (title, image) => {
+  picturePopupTitle.textContent = title.textContent;
+  picturePopupImage.src = image.src;
+  picturePopupImage.alt = image.alt;
+  handleOpenPopup(picturePopup);
 };
 //функция добавления карточки
 const addCard = (e) => {
   e.preventDefault();
-  createCard(inputCardName.value, inputCardLink.value);
-  clearInputs();
-  toggleClass(cardPopup, 'popup_opened');
+  const cardData = { name: inputCardName.value, link: inputCardLink.value };
+  renderCard(cardData);
+  formCard.reset();
+  handleClosePopup(cardPopup);
 };
 //функция создания карточки из шаблона
-const createCard = (title, link) => {
-  const template = document.querySelector('#card').content;
-  const card = template.querySelector('.card').cloneNode(true);
+const createCard = (item) => {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
   const cardTitle = card.querySelector('.card__title');
   const cardImage = card.querySelector('.card__image');
-  const like = card.querySelector('.card__like');
-  cardTitle.textContent = title;
-  cardImage.src = link;
-  cardImage.alt = title;
-  like.addEventListener('click', () => toggleClass(like, 'card__like_active'));
-  card.querySelector('.card__delete').addEventListener('click', deleteCard);
-  cardImage.addEventListener('click', () =>
-    openImagePopup(cardTitle, cardImage)
-  );
+  const cardLike = card.querySelector('.card__like');
+  const cardDelete = card.querySelector('.card__delete');
+
+  cardTitle.textContent = item.name;
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+
+  cardLike.addEventListener('click', () => cardLike.classList.toggle('card__like_active'));
+  cardDelete.addEventListener('click', handleDeleteCard);
+  cardImage.addEventListener('click', () => openPicturePopup(cardTitle, handlePictureClick));
+  
+  return card;
+};
+//функция добавления карточки на страницу
+const renderCard = (item) => {
+  const card = createCard(item);
   cardsList.prepend(card);
 };
 //функция удаления карточки
-const deleteCard = (e) => e.target.closest('.card').remove();
+const handleDeleteCard = (e) => e.target.closest('.card').remove();
 
 //слушатели событий
-openProfile.addEventListener('click', () => {
-  toggleClass(profilePopup, 'popup_opened');
-  addInputsValue();
+buttonOpenProfilePopup.addEventListener('click', () => {
+  handleOpenPopup(profilePopup);
+  addInputsData();
 });
-openCard.addEventListener('click', () => {
-  toggleClass(cardPopup, 'popup_opened');
+buttonOpenCardPopup.addEventListener('click', () => {
+  handleOpenPopup(cardPopup);
 });
-closePopups.forEach((item) =>
-  item.addEventListener('click', () =>
-    toggleClass(item.closest('.popup'), 'popup_opened')
-  )
+buttonClosePopups.forEach((item) =>
+  item.addEventListener('click', () => handleClosePopup(item.closest('.popup')))
 );
 formProfile.addEventListener('submit', changeProfile);
 formCard.addEventListener('submit', addCard);
 
-//массив изначальных карточек
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
-
 //добавляем массив изначальных карточек при загрузке на страницу
-initialCards.forEach((card) => {
-  createCard(card.name, card.link);
-});
+initialCards.forEach((item) => renderCard(item));
